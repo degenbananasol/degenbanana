841a2ec10ef240abc1ef91f9fc411db3070bdbc8
nurofsun <dev.nurahmat@gmail.com>
2021-05-28 14:17:22 +0700

    chore: add letter logo and change bg navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
+require('dotenv').config();
+
 module.exports = {
     purge: {
-        enabled: true,
+        enabled: process.env.MODE === 'development' ? false : true,
         content: ['*.html','./src/**/*.html']
     },
     darkMode: false, // or 'media' or 'class'
     theme: {
+        fontFamily: {
+            'sans': ['Open Sans', 'sans-serif']
+        },
         extend: {},
     },
     variants: {
